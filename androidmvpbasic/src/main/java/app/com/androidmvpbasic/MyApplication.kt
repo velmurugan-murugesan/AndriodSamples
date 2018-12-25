fbc27f85bae1e64fa4fafbb814cd78b117d424a3
velmurugan35 <velmurugan.murugesan@gmail.com>
2018-12-25 14:13:29 +0530

    Added application class

---
+++ androidmvpbasic/src/main/java/app/com/androidmvpbasic/MyApplication.kt
@@ -0,0 +1,11 @@
+package app.com.androidmvpbasic
+
+import android.app.Application
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ androidmvpbasic/src/main/java/app/com/androidmvpbasic/data/MovieRealm.kt
@@ -0,0 +1,3 @@
+package app.com.androidmvpbasic.data
+
+data class MovieRealm(val category: String,val imageUrl: String, val name: String, val desc: String)
